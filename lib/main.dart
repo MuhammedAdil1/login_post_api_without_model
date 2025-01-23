57b0f4b38ad902757ad7bd8aa135a4692d77406c
Adil-Muhammed-12 <adilx.2001@gmail.com>
2025-01-23 23:32:28 +0530

    Set repository language to Dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        //***********************************************
+        //***************************************
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         useMaterial3: false,
       ),
